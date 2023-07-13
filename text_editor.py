dcbd07f7d8aac35b4f31320cfd3fdc6639dc10b2
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-07-13 22:17:11 +0100

    text_editor: make 'open' button work

--- text_editor.py
+++ text_editor.py
@@ -1,5 +1,24 @@
 
 import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename, test
+
+
+def open_file():
+    filepath = askopenfilename(
+        filetypes=[
+            ("Text Files", "*.txt"),
+            ("All Files", "*.*")
+        ]
+    )
+    if not filepath:
+        return
+
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+
+    window.title(f"TEXT EDITOR - {filepath}")
 
 
 window = tk.Tk()
@@ -11,7 +30,7 @@ window.columnconfigure(1, weight=1, minsize=800)
 left_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=2)
 right_frame = tk.Frame(master=window)
 
-btn_open = tk.Button(text="OPEN", master=left_frame)
+btn_open = tk.Button(text="OPEN", master=left_frame, command=open_file)
 btn_save = tk.Button(text="SAVE AS", master=left_frame)
 
 btn_open.pack(fill=tk.X, padx=5, pady=5)
