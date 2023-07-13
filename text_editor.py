df3cd0960ae6121c027d860f2d3fb43f6b20d110
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-07-13 21:11:53 +0100

    text_editor: layout of the GUI

---
+++ text_editor.py
@@ -0,0 +1,26 @@
+
+import tkinter as tk
+
+
+window = tk.Tk()
+window.wm_title("TEXT EDITOR")
+
+window.rowconfigure(0, weight=1)
+window.columnconfigure(1, weight=1, minsize=800)
+
+left_frame = tk.Frame(master=window)
+right_frame = tk.Frame(master=window)
+
+btn_open = tk.Button(text="OPEN", master=left_frame)
+btn_save = tk.Button(text="SAVE AS", master=left_frame)
+
+btn_open.pack(fill=tk.X, padx=5, pady=5)
+btn_save.pack(fill=tk.X, padx=5, pady=5)
+
+txt_edit = tk.Text(master=right_frame)
+txt_edit.pack(fill=tk.BOTH, expand=True)
+
+left_frame.grid(row=0, column=0, sticky="nsew")
+right_frame.grid(row=0, column=1, sticky="nsew")
+
+window.mainloop()
